1af21c87efa5fab75a2368c96b16ccb8d71f0e7e
juaseo <juaseo96@gmail.com>
2022-05-11 21:37:57 +0900

    header 완성

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 /*************** 글로벌 설정 *****************/
 var auth = firebase.auth(); //firtbase의 auth(인증)모듈을 불러온다.
-var googleAuth = new firebase.auth.GoogleAuthProvider();// 구글 로그인 모듈을 불러온다.
+var googleAuth = new firebase.auth.GoogleAuthProvider(); // 구글 로그인 모듈을 불러온다.
 var user = null;
 
 /*************** 사용자 함수 *****************/
@@ -13,8 +13,22 @@ auth.onAuthStateChanged(onChangeAuth);
 
 /*************** 이벤트 콜백 *****************/
 function onChangeAuth(r) {
-user = r;
-console.log(user);
+	user = r;
+	console.log(r)
+	if (user) {
+		$('.header-wrapper .email').text(user.email);
+		$('.header-wrapper .photo img').attr('src', user.photoURL);
+		$('.header-wrapper .info-wrap').css('display', 'flex');
+		$('.bt-login').hide();
+		$('.bt-logout').show();
+	} 
+	else {
+		$('.header-wrapper .email').text('');
+		$('.header-wrapper .photo img').attr('src', '//via.placeholder.com/1x1/333');
+		$('.header-wrapper .info-wrap').css('display', 'none');
+		$('.bt-login').show();
+		$('.bt-logout').hide();
+	}
 }
 
 function onLogOut() {
